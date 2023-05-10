b4ba99d6e6c042d8c33889e7fe8ded184040c46a
omar jandali <omarjandali93@gmail.com>
2023-05-09 22:13:58 -0700

    Omar Jandali: added working gif search

--- app.js
+++ app.js
@@ -19,10 +19,28 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+const Tenor = require("tenorjs").client({
+  // Replace with your own key
+  "Key": "AIzaSyBkanXggye1Ws0_C65AS7Yma7rqYzA5TGA", // https://tenor.com/developer/keyregistration
+  "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+  "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+
 // Routes
 app.get('/', (req, res) => {
-  console.log(req.query) // => "{ term: hey" }[/bold]
-  res.render('home')
+  // Handle the home page when we haven't queried yet
+  term = ""
+  if (req.query.term) {
+      term = req.query.term
+  }
+  // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+  Tenor.Search.Query(term, "10")
+      .then(response => {
+          // store the gifs we get back from the search
+          const gifs = response;
+          // pass the gifs as an object into the home page
+          res.render('home', { gifs })
+      }).catch(console.error);
 })
 
 app.get('/greetings/:name', (req, res) => {
